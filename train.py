a599dcdc55a35f0d78d750f776ac5ae3c6751b6c
LiuXiaoyan97 <lxy15058247683@aliyun.com>
2024-05-30 14:09:47 +0800

    add ckpt and onlytest for args

--- train.py
+++ train.py
@@ -5,20 +5,32 @@ from MHBAMixerV2.utils import Text8DataModule, MHBAMixerV2Module
 import torch
 from MHBAMixerV2.config_MHBAMixerV2 import MHBAMixerV2Config
 from lightning.pytorch.callbacks import ModelCheckpoint
-
+import argparse
 torch.set_float32_matmul_precision('medium')
 torch.manual_seed(0)
 
 
-MHBAMixerV2config = MHBAMixerV2Config()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="funeturing from checkpoint")
+    parser.add_argument("--ckpt", type=str, default=None, help="model ckpt path for funeturing")
+    parser.add_argument("--onlytest", action="store_true", help="If you only want to perform a test, please enable this field.")
+    MHBAMixerV2config = MHBAMixerV2Config()
 
-checkpoint_callback = ModelCheckpoint(monitor="val_loss", 
-                                    filename='mixer-best-{epoch:03d}-{val_loss:.3f}',
-                                    save_top_k=1,
-                                    mode='min',
-                                    save_last=True
-                                    )
-trainer = L.Trainer(**MHBAMixerV2config.trainer, callbacks=[checkpoint_callback])
-dm = Text8DataModule(64)
-model = MHBAMixerV2Module(MHBAMixerV2config)
-trainer.fit(model, datamodule=dm)
+    checkpoint_callback = ModelCheckpoint(monitor="val_loss", 
+                                        filename='mixer-best-{epoch:03d}-{val_loss:.3f}',
+                                        save_top_k=1,
+                                        mode='min',
+                                        save_last=True
+                                        )
+    args = parser.parse_args()
+    if args.ckpt is not None:
+        trainer = L.Trainer(**MHBAMixerV2config.trainer, callbacks=[checkpoint_callback], ckpt_path=args.ckpt)
+        dm = Text8DataModule(MHBAMixerV2Config.batch_size)
+        model = MHBAMixerV2Module(MHBAMixerV2config)
+    if args.onlytest:
+        dm.setup("test")
+        trainer.test(model, datamodule=dm)
+    else:
+        trainer.fit(model, datamodule=dm)
+        
+
